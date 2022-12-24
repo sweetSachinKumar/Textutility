02b2aa48beaedb597c6f76ea4f832c68729ccb34
sweetSachinKumar <110831232+sweetSachinKumar@users.noreply.github.com>
2022-12-23 19:35:48 -0800

    best Utility website
    let your feeling to better version of your self

---
+++ build/hamburger.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelectorAll('.menu-button')
+let menu_burger = document.querySelectorAll('.menu-burger')
+
+menuBtn.addEventListener('click', ()=>{
+    menuBtn.classList.toggle('open');
+})
